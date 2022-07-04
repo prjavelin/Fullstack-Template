95b1e4dcbe83ddba84f5d31d6438d1531c79c539
prjavelin <prjavelin@gmai.com>
2022-07-04 13:46:55 -0400

    added port and mongo db not working atm

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'template',
+    dbName = 'star-wars-quotes',
     collection 
 
    MongoClient.connect(dbConnectionString)
    .then(client => {
     console.log('connected to Database')
     db = client.db(dbName)
-    collection = db.collection('sample')
+    collection = db.collection('quotes')
 
    }) 
+
+   app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server is running on port `)
+   })
+
