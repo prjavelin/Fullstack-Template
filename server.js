90b6d961831b514edf95f2301065438f4d773a0c
prjavelin <prjavelin@gmai.com>
2022-07-04 13:03:05 -0400

    added variables and mongo collection

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'template',
+    collection 
+
+   MongoClient.connect(dbConnectionString)
+   .then(client => {
+    console.log('connected to Database')
+    db = client.db(dbName)
+    collection = db.collection('sample')
+
+   }) 
