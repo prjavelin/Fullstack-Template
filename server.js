966ff62c4bd17347e28ef315143bb721f82bd48b
prjavelin <prjavelin@gmai.com>
2022-07-05 17:31:12 -0400

    added middleware

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'star-wars-quotes',
+    dbName = 'template',
     collection 
 
-   MongoClient.connect(dbConnectionString)
+MongoClient.connect(dbConnectionString)
    .then(client => {
     console.log('connected to Database')
     db = client.db(dbName)
-    collection = db.collection('quotes')
+    collection = db.collection('sample')
 
    }) 
 
-   app.listen(process.env.PORT || PORT, ()=>{
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server is running on port `)
    })
 
